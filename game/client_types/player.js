b996730d87e707b4057f77ae29398994bda33fa6
teymess <tillmann.eymess@awi.uni-heidelberg.de>
2022-04-21 10:09:58 +0200

    altruism2 sentence inverted

--- game/client_types/player.js
+++ game/client_types/player.js
@@ -1931,7 +1931,7 @@ module.exports = function(treatmentName, settings, stager, setup, gameRoom) {
                     {
                         id: 'general_altruism',
                         orientation: 'H',
-                        mainText: '<span style="font-weight: normal;color:gray;">Q20</span> How do you assess your willingness to share with others without expecting anything in return when it comes to a good cause?',
+                        mainText: '<span style="font-weight: normal;color:gray;">Q20</span> When it comes to a good cause, how do you assess your willingness to share with others without expecting anything in return?',
                         left: 'Completely unwilling to share',
                         right: 'Very willing to share',
                         choices: [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
